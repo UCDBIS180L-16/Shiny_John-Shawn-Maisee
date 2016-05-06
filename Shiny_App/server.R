5ce2b484be4a597041e1222e45d8a3da8f356a57
bis180l_local <johnny3420@github.com>
2016-05-05 17:03:10 -0700

    outline

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -1,8 +1,10 @@
 # server.R
+# Traits protein content, amylose content, blast resistance, 
+# plant height, panicle length, flag leaf length, alkali spreading value
 library(shiny)
 library(ggplot2)
 library(reshape2)
-
+load("data/rice_shiny_data")
 shinyServer(
   function(input, output) {
   }
