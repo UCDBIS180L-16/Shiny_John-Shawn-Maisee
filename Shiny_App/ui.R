d6f97f433acba4f2edc39d5f250007cba3b221e0
maiseeyang <maiyang@ucdavis.edu>
2016-05-06 13:41:23 -0700

    test

--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -5,9 +5,27 @@ shinyUI(fluidPage(
   titlePanel("Interactive Viewer of Rice Phenotypes"),
   sidebarLayout(
     sidebarPanel(
+      radioButtons("plot",
+                   "Choose a plot to display:",
+                   c("histogram",
+                     "boxplot",
+                     "violin plot")),
+   
+      selectInput("trait", 
+                  label = "Choose a trait to display",
+                  choices = list("Alkali Spreading Value" = "Alkali.spreading.value",
+                                 "Amylose Content" = "Amylose.content",
+                                 "Blast Resistance"= "Blast.resistance",
+                                 "Flag Leaf Length" = "Flag.leaf.length",
+                                 "Panicle Length" = "Panicle.length",
+                                 "Plant Height" = "Plant.height",
+                                 "Protein Content" = "Protein.content")),
+     actionButton("Button", "Button")),
+    
+
+
+    mainPanel(plotOutput("boxplot"))
       
-    ),
-    mainPanel()
-  )
+)
 ))
-  
+
