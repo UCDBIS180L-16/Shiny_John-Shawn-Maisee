f0baab8f1553af4251440dfcd01706d621d1fce5
bis180l_local <johnny3420@github.com>
2016-05-10 15:10:48 -0700

    Fixed typos and made histograms less ugly

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -10,39 +10,39 @@ shinyServer(
     # output name boxplot
     plant <- read.csv("data/shiny.data")
     plant$popID <- as.character(as.numeric(plant$popID))
-      pl <- reactiveValues(data = NULL)
-      lp <- reactiveValues(data = NULL)
-      
-      observeEvent(input$anc, {
-        pl$data <- ggplot(data = plant,
-                              aes_string(x = "popID", fill = "popID"))
-        lp$data <- ggplot(data = plant,
-                          aes_string(fill = "popID")) + facet_grid(~popID) 
-      })
-      observeEvent(input$reg, {
-          pl$data <- ggplot(data = plant,
-                            aes_string(x = "Region", fill = "Region"))
-          lp$data <- ggplot(data = plant,
-                            aes_string(fill = "Region")) + facet_grid(~Region)
-          
-        })
-        
-      output$boxplot <- renderPlot({
-        if (is.null(pl$data)) {
-          ggplot(data = plant, aes_string(x = input$trait, fill = "Region")) + facet_grid(~Region) + geom_histogram()
-          pl$data <- ggplot(data = plant,
-                            aes_string(x = "Region", fill = "Region"))
-          lp$data <- ggplot(data = plant,
-                            aes_string(fill = "Region")) + facet_grid(~Region)
-        }
-        else if (input$plot == "violin plot"){
-          pl$data + geom_violin(aes_string(y = input$trait))
-        } else if (input$plot == "boxplot"){
-          pl$data + geom_boxplot(aes_string(y = input$trait))
-        } else if (input$plot == "histogram"){
-          lp$data + geom_histogram(aes_string(x = input$trait))
-        }
-      })
-})
+    pl <- reactiveValues(data = NULL)
+    lp <- reactiveValues(data = NULL)
     
+    observeEvent(input$anc, {
+      pl$data <- ggplot(data = plant,
+                        aes_string(x = "popID", fill = "popID"))
+      lp$data <- ggplot(data = plant,
+                        aes_string(fill = "popID")) + facet_wrap(~popID, ncol = 2) 
+    })
+    observeEvent(input$reg, {
+      pl$data <- ggplot(data = plant,
+                        aes_string(x = "Region", fill = "Region"))
+      lp$data <- ggplot(data = plant,
+                        aes_string(fill = "Region")) + facet_wrap(~Region, ncol = 3)
+      
+    })
+    
+    output$boxplot <- renderPlot({
+      if (is.null(pl$data)) {
+        ggplot(data = plant, aes_string(x = input$trait, fill = "Region")) + facet_wrap(~Region, ncol = 3) + geom_histogram()
+        pl$data <- ggplot(data = plant,
+                          aes_string(x = "Region", fill = "Region"))
+        lp$data <- ggplot(data = plant,
+                          aes_string(fill = "Region")) + facet_wrap(~Region, ncol = 3)
+      }
+      else if (input$plot == "violin plot"){
+        pl$data + geom_violin(aes_string(y = input$trait))
+      } else if (input$plot == "boxplot"){
+        pl$data + geom_boxplot(aes_string(y = input$trait))
+      } else if (input$plot == "histogram"){
+        lp$data + geom_histogram(aes_string(x = input$trait))
+      }
+    })
+  })
+
 # output name boxplot
--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -4,13 +4,13 @@ library(shiny)
 shinyUI(fluidPage(
   titlePanel("Interactive Viewer of Rice Phenotypes"),
   helpText("This web application was designed in order to allow users to visualize different phenotypic aspects among over 400 different 
-           varieties of rice form around the world. With this webapp, one can assess variation among the accessions based on traits ranging 
+           varieties of rice from around the world. With this webapp, one can assess variation among the accessions based on traits ranging 
            from Panicle Length to Protein Content. Trait variation among all rice accessions within the entire population can be further assessed 
            based on ancestral population membership (determined by Multi-Dimensional-Scaling of a SNP dataset covering over 40,000 SNP markers from 
            the rice genome), or by the geographical region in which they were found to naturally occur. To use the app, use the Radio Button
            widget to select the type of plot desired for visualizing the data. Next, use the Select Input drop down widget to choose the
            phenotypic trait you want to visualize. Finally, choose whether you wish to visualize the data based on either the geographic region 
-           or ancestral population membership of each rice accession by clicking either of the two Action Buttons at the bottom of the side pannel. 
+           or ancestral population membership of each rice accession by clicking either of the two Action Buttons at the bottom of the side panel. 
            The plot will automatically render and change when a different plot type is specified, a different phenotypic trait is selected, or when
            toggling back and forth between ancestral or region based grouping. The default plot shows a histogram for the Alkalai Spreading Value data
            where all rice accessions are grouped together based on their indigenous region."),
