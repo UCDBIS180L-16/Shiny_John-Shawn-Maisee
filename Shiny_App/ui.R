ca4e2afecd6d985ee1edec61c4238c17876396f1
smhigdon88 <smhigdon@ucdavis.edu>
2016-05-10 10:32:48 -0700

    modified help text on ui.R

--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -3,7 +3,15 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("Interactive Viewer of Rice Phenotypes"),
-  helpText("This web application was designed in order to allow users to visualize different phenotypic aspects among over 400 different varieties of rice form around the world. With this webapp, one can assess variation among the accessions based on traits ranging from Panicle Length to Protein Content. Variation for each trait within the entire population can be further assessed based on ancestral population structure."),
+  helpText("This web application was designed in order to allow users to visualize different phenotypic aspects among over 400 different 
+           varieties of rice form around the world. With this webapp, one can assess variation among the accessions based on traits ranging 
+           from Panicle Length to Protein Content. Trait variation among all rice accessions within the entire population can be further assessed 
+           based on ancestral population membership (determined by Multi-Dimensional-Scaling of a SNP dataset covering over 40,000 SNP markers from 
+           the rice genome), or by the geographical region in which they were found to naturally occur. To use the app, use the Radio Button
+           widget to select the type of plot desired for visualizing the data. Next, using the Select Input drop down widget to choose the
+           phenotypic trait you want to visualize. Finally, choose whether you wise to visualize the data based on either the geographic region 
+           or ancestral population membership of each rice accession using the Action Button. The plot will automatically render and change when 
+           the type of plot is modified, but an action button must be clicked when the phenotypic trait selection is switched."),
     
   sidebarLayout(
     sidebarPanel(
@@ -26,9 +34,7 @@ shinyUI(fluidPage(
 
      actionButton("reg", "Region"),
      actionButton("anc", "Ancestral")),
-
-
-
+    
     mainPanel(plotOutput("boxplot"))
       
 )
